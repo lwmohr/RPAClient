85fdd63ee12b6e5e5eaa88f9800338251b0b565b
Lucas Mohr <lmohr78@gmail.com>
2011-09-18 14:53:44 -0500

    commit

--- MyBot/MyBotCSharp/MyBot.cs
+++ MyBot/MyBotCSharp/MyBot.cs
@@ -1,142 +1,76 @@
 ﻿using RockPaperScissorsPro;
 using System.Collections;
+using System;
 
 namespace RockPaperAzure
 {
     public class MyBot : IRockPaperScissorsBot
     {
-        int tiesCounter = 0;
 
         bool firstTime = true;
-        bool twoTieWater = false;
-        bool waterAfterTie = false;
-        bool firstTieDyna = false;
-        bool secondTieDyna = false;
-        int dynaTwice, dynaSingleWater, dynaTwiceWater, waterSingle, transitionPoint;
         BotLogger MyBotLog = new BotLogger();
         public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
         {
-            MyBotLog.addLog("asdfxasdf","asdfasdf",4,6);
-            if (firstTime)
+            
+            if (!firstTime)
             {
-                dynaTwice = Moves.GetRandomNumber(500);
-                dynaSingleWater = Moves.GetRandomNumber(500);
-                dynaTwiceWater = Moves.GetRandomNumber(500);
-                //dynaTwiceWater = 5;
-                waterSingle = Moves.GetRandomNumber(500);
+                int myMove, opponentMove;
+                if (you.LastMove.Equals(Moves.WaterBalloon))
+                    myMove = 0;
+                else if (you.LastMove.Equals(Moves.Dynamite))
+                    myMove = 1;
+                else 
+                    myMove = 3;
+                if (opponent.LastMove.Equals(Moves.WaterBalloon))
+                    opponentMove = 0;
+                else if (opponent.LastMove.Equals(Moves.Dynamite))
+                    opponentMove = 1;
+                else 
+                    opponentMove = 3;
+                MyBotLog.addLog(myMove,opponentMove);
+                
+            }
+            else
                 firstTime = false;
+
+            int history;
+            int dynamite = 0;
+            int water = 0;
+            int randomNum;
+            int tiesRemaining;
+
+            if (you.LastMove.Equals(opponent.LastMove) || (opponent.LastMove.Equals(Moves.Dynamite) || opponent.LastMove.Equals(Moves.WaterBalloon)))
+            {
+                history = 5;
+                MyBotLog.analyzeThrow(history, ref dynamite, ref water);
+                if ((dynamite >= 3) && you.HasDynamite)
+                {
+                        you.Log.AppendLine(String.Format("{0}: Water Balloon : {1}/{2}", you.NumberOfDecisions, dynamite, history));
+                        return Moves.WaterBalloon;
+                    }
+                if (water >= 3)
+                {
+                    you.Log.AppendLine(String.Format("{0}: Random : {1}/{2}", you.NumberOfDecisions, water, history));
+                    return Moves.GetRandomMove();
+
+                }
             }
 
-            //float highPoint = you.Points;
-            //float totalThrows = you.NumberOfDecisions;
-            //float remainingDynamite = you.DynamiteRemaining;
-            //if (you.Points < opponent.Points)
-            //    highPoint = opponent.Points;
-            //float winPercent = highPoint / totalThrows;
-            //float predictedThrows = (float)1000 / winPercent;
-            //float remainingThrows = predictedThrows - totalThrows;
-            //float oddsForDynamite = remainingDynamite / remainingThrows;
-
-            //if ((dynaTwice == you.NumberOfDecisions) || ((dynaTwice + 1) == you.NumberOfDecisions))
-            //    return Moves.Dynamite;
-            //if (dynaSingleWater == you.NumberOfDecisions)
-            //    return Moves.Dynamite;
-            //if ((dynaSingleWater + 1) == you.NumberOfDecisions)
-            //    return Moves.WaterBalloon;
-            //if ((dynaTwiceWater == you.NumberOfDecisions) || ((dynaTwiceWater + 1) == you.NumberOfDecisions))
-            //    return Moves.Dynamite;
-            //if ((dynaTwiceWater + 2) == you.NumberOfDecisions)
-            //    return Moves.WaterBalloon;
-            //if (waterSingle == you.NumberOfDecisions)
-            //    return Moves.WaterBalloon;
-
-            if (tiesCounter >= 2 && opponent.LastMove == Moves.WaterBalloon)
-                twoTieWater = true;
-            if ((tiesCounter == 1) && (opponent.LastMove == Moves.Dynamite))
-                firstTieDyna = true;
-            if ((tiesCounter == 2) && (opponent.LastMove == Moves.Dynamite))
-                secondTieDyna = true;
-            if ((you.LastMove == opponent.LastMove) && (opponent.LastMove == Moves.WaterBalloon))
-                waterAfterTie = true;
-
-            if (you.LastMove == opponent.LastMove)
-                tiesCounter++;
-            else
-                tiesCounter = 0;
-
-            if (you.HasDynamite)
+            if ((you.LastMove.Equals(opponent.LastMove)) && you.HasDynamite)
             {
-                if (tiesCounter > 0)
+                randomNum = Moves.GetRandomNumber(3);
+                if ((MyBotLog.getTies() > 1) && ( randomNum == 2))
                 {
-                    int myRand = Moves.GetRandomNumber(1000);
-                    switch (tiesCounter){
-
-                        case 1:
-                            //if ((transitionPoint > you.NumberOfDecisions) && opponent.HasDynamite)
-                            //{
-                                if ((myRand < 500) && (you.DynamiteRemaining >= Moves.GetRandomNumber((int)(rules.PointsToWin - opponent.Points) / 2) +1))
-                                    if ((rules.PointsToWin - opponent.Points) < 100)
-                                        return Moves.Dynamite;
-                                //if ((firstTieDyna) && ((myRand > 950) && (opponent.DynamiteRemaining > 1)))
-                                //    return Moves.WaterBalloon;
-
-                            //}
-                            break;
-                        case 2:
-                            if ((you.LastMove == Moves.Dynamite) && (opponent.DynamiteRemaining > 1))
-                            {
-                                if ((myRand < 450) && (you.DynamiteRemaining >= Moves.GetRandomNumber((int)((rules.PointsToWin - opponent.Points) / 5) +1)))
-                                    return Moves.Dynamite;
-                                if (myRand > 950)
-                                    return Moves.WaterBalloon;
-
-                            }
-                            else if (you.DynamiteRemaining >= Moves.GetRandomNumber((int)((rules.PointsToWin - opponent.Points) / 5) +1))
-                            {
-                                if (myRand < 500)
-                                    return Moves.Dynamite;
-                                //if ((secondTieDyna) && ((myRand > 950) && (opponent.DynamiteRemaining > 1)))
-                                //    return Moves.WaterBalloon;
-                            }
-                            break;
-                        default:
-                            if ((!waterAfterTie) && (opponent.DynamiteRemaining < 2))
-                            {
-                                //if (myRand < 750)
-                                    return Moves.Dynamite;
-                                //else if ((myRand > 500) && (opponent.DynamiteRemaining > 1))
-                                //    return Moves.WaterBalloon;
-
-                            }
-                            else if (!waterAfterTie)
-                            {
-                                if (myRand < 500)
-                                    return Moves.Dynamite;
-                                else if ((myRand > 500) && (opponent.DynamiteRemaining > 1))
-                                    return Moves.WaterBalloon;
-
-                            }
-
-                            else if ((you.LastMove == Moves.Dynamite) && (opponent.DynamiteRemaining > 1))
-                            {
-                                if ((myRand < 450) && (you.DynamiteRemaining >= Moves.GetRandomNumber((int)((rules.PointsToWin - opponent.Points) / 11) +1)))
-                                    return Moves.Dynamite;
-                                if (myRand > 950) 
-                                    return Moves.WaterBalloon;
-
-                            }
-                            else
-                            {
-                                if ((myRand < 500) && (you.DynamiteRemaining >= Moves.GetRandomNumber((int)((rules.PointsToWin - opponent.Points) / 11) +1)))
-                                    return Moves.Dynamite;
-                                if ((secondTieDyna) && ((myRand > 950) && (opponent.DynamiteRemaining > 1)))
-                                    return Moves.WaterBalloon;
-                            }
-                            break;
-                    }
-                }
-                if ((you.DynamiteRemaining >= Moves.GetRandomNumber(((rules.PointsToWin - opponent.Points) * 16) - 14)) && ((rules.PointsToWin - opponent.Points) < 20))
+                    you.Log.AppendLine(String.Format("{0}: Multiple ties : {1},{2}", you.NumberOfDecisions, MyBotLog.getTies(),randomNum));
                     return Moves.Dynamite;
+                }
+                tiesRemaining = ((2 * (rules.PointsToWin - opponent.Points)) / 3);
+                randomNum = Moves.GetRandomNumber(tiesRemaining);
+                if (randomNum <= you.DynamiteRemaining)
+                {
+                    you.Log.AppendLine(String.Format("{0}: Single Tie : {1},{2}", you.NumberOfDecisions, MyBotLog.getTies(),tiesRemaining));
+                    return Moves.Dynamite;
+                }
             }
             return Moves.GetRandomMove();
         }
@@ -144,49 +78,54 @@ namespace RockPaperAzure
 
     public class BotLogger
     {
-        ArrayList logArray = new ArrayList();
-	List<int> ShootInfo = new List<int>();
-	private int[,] myArray = new int[3000,5];
-        private list ShootInfo
-        {
-            private string myThrow { get; set; }
-            private string opponentThrow { get; set; }
-            private int currentTies { public get; set; }
-            private int pointsWon { get; set; }
+    //    ArrayList logArray = new ArrayList();
+    //List<int> ShootInfo = new List<int>();
+	    private int[,] myArray = new int[3000,5];
+        int currentTies = 0;
+        int pointsWon = 0;
+        int throwNum = 0;
 
-            public ShootInfo(string myThrow, string opponentThrow, int currentTies, int pointsWon)
+
+        public void addLog(int myThrow, int opponentThrow)
+        {
+            throwNum++;
+            if (myThrow == opponentThrow)
+                currentTies++;
+            else
             {
-                this.myThrow = myThrow;
-                this.opponentThrow = opponentThrow;
-                this.currentTies = currentTies;
-                this.pointsWon = pointsWon;
+                pointsWon = currentTies + 1;
+                currentTies = 0;
             }
+
+            myArray[throwNum - 1,0] = myThrow;
+            myArray[throwNum - 1,1] = opponentThrow;
+            myArray[throwNum - 1,2] = currentTies;
+            myArray[throwNum - 1,3] = pointsWon;
+            myArray[throwNum - 1,4] = throwNum;
         }
 
-        public void addLog(int myThrow, int opponentThrow, int currentTies, int pointsWon, int throwNum)
+        public int getTies()
         {
-            myArray[throwNum - 1][0] = myThrow;
-            myArray[throwNum - 1][1] = opponentThrow;
-            myArray[throwNum - 1][2] = currentTies;
-            myArray[throwNum - 1][3] = pointsWon;
-            myArray[throwNum - 1][4] = throwNum;
+            return currentTies;
         }
 
-        public void addLog(string myThrow, string opponentThrow, int currentTies, int pointsWon)
+        public void analyzeThrow(int histInstances, ref int dynamite, ref int water)
         {
-            logArray.Add(new ShootInfo(myThrow, opponentThrow, currentTies, pointsWon));
-        }
-
-        //int i = (ShootInfo)logArray[j].get.currentTies;
-        public void getShoot()
-        {
-            int j = 5;
-            int num;
-            ShootInfo[] MyLogArray = logArray.ToArray(typeof(ShootInfo)) as ShootInfo[];
-            foreach (ShootInfo MyLog in MyLogArray)
+            dynamite = 0;
+            water = 0;
+            int tmpTies = myArray[throwNum - 1,2];
+            for (int i = (throwNum - 2); i >= 0; i--)
             {
-                num = MyLog(typeof)ShootInfo(j);
-                
+                if (histInstances == 0)
+                    break;
+                if ((myArray[i,0] == myArray[throwNum - 1,0]) && (myArray[i,2] == myArray[throwNum - 1,2]))
+                {
+                    if (myArray[i + 1,1] == 0)
+                        water++;
+                    else if (myArray[i + 1,1] == 1)
+                        dynamite++;
+                    histInstances--;
+                }
             }
         }
 
