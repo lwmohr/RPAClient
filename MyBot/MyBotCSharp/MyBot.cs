4bdfb2d34ad652f296f22b3f60d8d0716bb8c6b5
Lucas Mohr <lmohr78@gmail.com>
2011-09-18 14:52:34 -0500

    commit

--- MyBot/MyBotCSharp/MyBot.cs
+++ MyBot/MyBotCSharp/MyBot.cs
@@ -1,26 +1,22 @@
 ﻿using RockPaperScissorsPro;
-using System;
+using System.Collections;
 
 namespace RockPaperAzure
 {
     public class MyBot : IRockPaperScissorsBot
     {
-        // Random sample implementation
-        //public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
-        //{
-        //     return Moves.GetRandomMove();
-        //}
         int tiesCounter = 0;
 
         bool firstTime = true;
         bool twoTieWater = false;
+        bool waterAfterTie = false;
         bool firstTieDyna = false;
         bool secondTieDyna = false;
         int dynaTwice, dynaSingleWater, dynaTwiceWater, waterSingle, transitionPoint;
-
-
+        BotLogger MyBotLog = new BotLogger();
         public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
         {
+            MyBotLog.addLog("asdfxasdf","asdfasdf",4,6);
             if (firstTime)
             {
                 dynaTwice = Moves.GetRandomNumber(500);
@@ -29,8 +25,6 @@ namespace RockPaperAzure
                 //dynaTwiceWater = 5;
                 waterSingle = Moves.GetRandomNumber(500);
                 firstTime = false;
-                transitionPoint = Moves.GetRandomNumber(400);
-                transitionPoint += 700;
             }
 
             //float highPoint = you.Points;
@@ -62,7 +56,8 @@ namespace RockPaperAzure
                 firstTieDyna = true;
             if ((tiesCounter == 2) && (opponent.LastMove == Moves.Dynamite))
                 secondTieDyna = true;
-
+            if ((you.LastMove == opponent.LastMove) && (opponent.LastMove == Moves.WaterBalloon))
+                waterAfterTie = true;
 
             if (you.LastMove == opponent.LastMove)
                 tiesCounter++;
@@ -79,116 +74,111 @@ namespace RockPaperAzure
                         case 1:
                             //if ((transitionPoint > you.NumberOfDecisions) && opponent.HasDynamite)
                             //{
-                                if ((myRand < 500) && (you.DynamiteRemaining >= Moves.GetRandomNumber((int)(rules.PointsToWin - opponent.Points) / 2)))
-                                    return Moves.Dynamite;
-                            //if ((firstTieDyna) && ((myRand > 950) && (opponent.HasDynamite)))
-                            //    return Moves.WaterBalloon;
+                                if ((myRand < 500) && (you.DynamiteRemaining >= Moves.GetRandomNumber((int)(rules.PointsToWin - opponent.Points) / 2) +1))
+                                    if ((rules.PointsToWin - opponent.Points) < 100)
+                                        return Moves.Dynamite;
+                                //if ((firstTieDyna) && ((myRand > 950) && (opponent.DynamiteRemaining > 1)))
+                                //    return Moves.WaterBalloon;
 
                             //}
                             break;
                         case 2:
                             if ((you.LastMove == Moves.Dynamite) && (opponent.DynamiteRemaining > 1))
                             {
-                                if (myRand < 200)
+                                if ((myRand < 450) && (you.DynamiteRemaining >= Moves.GetRandomNumber((int)((rules.PointsToWin - opponent.Points) / 5) +1)))
                                     return Moves.Dynamite;
-                                if (myRand > 800)
+                                if (myRand > 950)
                                     return Moves.WaterBalloon;
 
                             }
-                            else if (you.DynamiteRemaining >= Moves.GetRandomNumber((int)(rules.PointsToWin - opponent.Points) / 5))
+                            else if (you.DynamiteRemaining >= Moves.GetRandomNumber((int)((rules.PointsToWin - opponent.Points) / 5) +1))
                             {
                                 if (myRand < 500)
                                     return Moves.Dynamite;
-                                //if ((secondTieDyna) && ((myRand > 990) && (opponent.HasDynamite)))
+                                //if ((secondTieDyna) && ((myRand > 950) && (opponent.DynamiteRemaining > 1)))
                                 //    return Moves.WaterBalloon;
                             }
                             break;
                         default:
-                            if ((you.LastMove == Moves.Dynamite) && (opponent.DynamiteRemaining > 1))
+                            if ((!waterAfterTie) && (opponent.DynamiteRemaining < 2))
                             {
-                                if (myRand < 200)
+                                //if (myRand < 750)
                                     return Moves.Dynamite;
-                                if (myRand > 800) 
+                                //else if ((myRand > 500) && (opponent.DynamiteRemaining > 1))
+                                //    return Moves.WaterBalloon;
+
+                            }
+                            else if (!waterAfterTie)
+                            {
+                                if (myRand < 500)
+                                    return Moves.Dynamite;
+                                else if ((myRand > 500) && (opponent.DynamiteRemaining > 1))
+                                    return Moves.WaterBalloon;
+
+                            }
+
+                            else if ((you.LastMove == Moves.Dynamite) && (opponent.DynamiteRemaining > 1))
+                            {
+                                if ((myRand < 450) && (you.DynamiteRemaining >= Moves.GetRandomNumber((int)((rules.PointsToWin - opponent.Points) / 11) +1)))
+                                    return Moves.Dynamite;
+                                if (myRand > 950) 
                                     return Moves.WaterBalloon;
 
                             }
                             else
                             {
-                                if (myRand < 500)
+                                if ((myRand < 500) && (you.DynamiteRemaining >= Moves.GetRandomNumber((int)((rules.PointsToWin - opponent.Points) / 11) +1)))
                                     return Moves.Dynamite;
-                                //if ((secondTieDyna) && ((myRand > 990) && (opponent.HasDynamite)))
-                                //    return Moves.WaterBalloon;
+                                if ((secondTieDyna) && ((myRand > 950) && (opponent.DynamiteRemaining > 1)))
+                                    return Moves.WaterBalloon;
                             }
                             break;
                     }
                 }
-                if (you.DynamiteRemaining >= Moves.GetRandomNumber(((rules.PointsToWin - opponent.Points) * 16) - 14))
+                if ((you.DynamiteRemaining >= Moves.GetRandomNumber(((rules.PointsToWin - opponent.Points) * 16) - 14)) && ((rules.PointsToWin - opponent.Points) < 20))
                     return Moves.Dynamite;
             }
             return Moves.GetRandomMove();
         }
+    }
 
-        // Cycle sample implementation
-        //public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
-        //{
-        //    if (you.LastMove == Moves.Rock)
-        //        return Moves.Paper;
+    public class BotLogger
+    {
+        ArrayList logArray = new ArrayList();
+        private class ShootInfo
+        {
+            private string myThrow { get; set; }
+            private string opponentThrow { get; set; }
+            private int currentTies { public get; set; }
+            private int pointsWon { get; set; }
 
-        //    if (you.LastMove == Moves.Paper)
-        //        return Moves.Scissors;
+            public ShootInfo(string myThrow, string opponentThrow, int currentTies, int pointsWon)
+            {
+                this.myThrow = myThrow;
+                this.opponentThrow = opponentThrow;
+                this.currentTies = currentTies;
+                this.pointsWon = pointsWon;
+            }
+        }
 
-        //    if (you.LastMove == Moves.Scissors)
-        //        if (you.HasDynamite)
-        //            return Moves.Dynamite;
-        //        else
-        //            return Moves.WaterBalloon;
+        public void addLog(string myThrow, string opponentThrow, int currentTies, int pointsWon)
+        {
+            logArray.Add(new ShootInfo(myThrow, opponentThrow, currentTies, pointsWon));
+            return;
+        }
 
-        //    if (you.LastMove == Moves.Dynamite)
-        //        return Moves.WaterBalloon;
+        //int i = (ShootInfo)logArray[j].get.currentTies;
+        public void getShoot()
+        {
+            int j = 5;
+            int num;
+            ShootInfo[] MyLogArray = logArray.ToArray(typeof(ShootInfo)) as ShootInfo[];
+            foreach (ShootInfo MyLog in MyLogArray)
+            {
+                num = MyLog(typeof)ShootInfo(j);
+                
+            }
+        }
 
-        //    return Moves.Rock;
-        //}
-
-        // BigBang sample implementation
-        //public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
-        //{
-        //    if (you.NumberOfDecisions < 5)
-        //        return Moves.Dynamite;
-        //    else
-        //        return Moves.GetRandomMove();
-        //}
-        //public Move MakeMove(IPlayer you, IPlayer opponent, GameRules rules)
-        //{
-        //    float highPoint = you.Points;
-        //    float totalThrows = you.NumberOfDecisions;
-        //    float remainingDynamite = you.DynamiteRemaining;
-        //    if (you.Points < opponent.Points)
-        //        highPoint = opponent.Points;
-        //    float winPercent = highPoint / totalThrows;
-        //    float predictedThrows = 1000 / winPercent;
-        //    float remainingThrows = predictedThrows - totalThrows;
-        //    float oddsForDynamite = remainingDynamite / remainingThrows;
-
-
-        //    if (opponent.LastMove == Moves.Dynamite)
-        //        hisDynamite--;
-        //    if (you.LastMove == Moves.Dynamite)
-        //        myDynamite--;
-        //    if (you.HasDynamite && opponent.HasDynamite)
-        //    {
-        //        int myRand = Moves.GetRandomNumber(19);
-        //        if (myRand == 18)
-        //            return Moves.Dynamite;
-        //        return Moves.GetRandomMove();
-        //    }
-        //    if (you.HasDynamite && !opponent.HasDynamite)
-        //    {
-        //        int myRand = Moves.GetRandomNumber(5);
-        //        if (myRand == 3)
-        //            return Moves.Dynamite;
-        //        return Moves.GetRandomMove();
-        //    }
-        //    return Moves.GetRandomMove();
-        //}
     }
 }
